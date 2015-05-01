4754c224573dec214ddb28b9698b4af527d31d12
senner007 <nielshtg@hotmail.com>
2015-05-02 00:53:33 +0200

    drag and drop with delete function

--- js/jumbleScramble.js
+++ js/jumbleScramble.js
@@ -7,22 +7,23 @@
 		return support;
 	})();
 	
-	function addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o) {
+	function addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o, thisContainer) {
 			
 			eltsObject[divId][n] = elt;
 			eltsObject[divId][n].completeWidth = $thisWidth || 0;								// its size (with the margin)
 			eltsObject[divId][n].completeHeight = $thisHeight || 0;							// its height (with the margin)
 			eltsObject[divId][n].pos = getOffset(elt);												// its position (left and top position)
-			eltsObject[divId][n].pos.left = 0;
+			o.isVertical == true  ? eltsObject[divId][n].pos.left = 0: eltsObject[divId][n].pos.left = eltsObject[divId][n].pos.left;
 			eltsObject[divId][n].initialN = n;														// its initial position (as per the other elements)
 			eltsObject[divId][n].n = n;																// its current position (as per the other elements)
 			eltsObject[divId][n].o = o;																// its current position (as per the other elements)
 			eltsObject[divId][n].moved = false;	
+			eltsObject[divId][n].belongsTo = thisContainer;	
 	};
 	
 	function addHandlers(eltsObject, divId, elt, o, div) {
 			var parentContainment = ( $('#frame').length ? $('#frame') : div );		// use '.#frame' if it exists
-			if (o.isHorizontal ? axis = 'y' : axis = 'false')
+			if (o.isVertical ? axis = 'y' : axis = 'false')
 			elt.draggable({															// make the element draggable
 				iframeFix: true,
 				addClasses: false,
@@ -52,7 +53,7 @@
 	};
 	
 	function onStart(elt, o) {																// Start			
-		var	inAnim = ( o.isHorizontal && transSupport ? ({'box-shadow': '0px 2px 10px rgba(0,0,0,.7)'}) : ({'opacity': 0.4, 'z-index':200}) );				
+		var	inAnim = ( o.isVertical && transSupport ? ({'box-shadow': '0px 2px 10px rgba(0,0,0,.7)'}) : ({'opacity': 0.4, 'z-index':200}) );				
 		elt.css(inAnim);
 	};
 	
@@ -67,7 +68,7 @@
 		return closest; // return the value
 	}
 	
-	function closest2(arr, closestTo){
+	/* function closest2(arr, closestTo){
 
 		var closest = Math.min.apply(null, arr); //Get the highest number in arr in case it match nothing.
 
@@ -76,7 +77,7 @@
 		}
 	
 		return closest; // return the value
-	}
+	} */
  	
 	
 	var nToAnimate
@@ -87,50 +88,38 @@
 	function onDrag(e, ui, elt, elts, o){													// Drag
 		
 		var thisElt = this;																//must be saved to a variable to avoid random 
-																					//occurrences of non-moving elements in safari iPad.
+																				//occurrences of non-moving elements in safari iPad.
 		var oldPos = (thisElt.eltPos != null ? thisElt.eltPos : ui.position); 			//find the old position stored on the $object 
 		thisElt.eltPos = ui.position;													//its current position derived from $draggable object		
-		//console.log(thisElt.eltPos.left)
-		
-		//console.log(elt)
-		
-		eltParentId = elt[0].parentElement.parentElement.attributes.id.nodeValue;
-		adjacentParentId = (eltParentId == 'jMyPuzzleId1' ? eltsObject.jMyPuzzleId0 : eltsObject.jMyPuzzleId1);
+
+		adjacentParentId = (elt.belongsTo % 2 == 0 && Object.keys(eltsObject).length > 1 ?  eltsObject.jMyPuzzleId1 : eltsObject.jMyPuzzleId0);
 		adjacentDir = adjacentParentId[0].parent().offset().left -  elt.parent().offset().left;	
-		var dirSwitch = (eltParentId == 'jMyPuzzleId1' ? thisElt.eltPos.left < adjacentDir/2 :thisElt.eltPos.left > adjacentDir/2); 
+		var dirSwitch = (elt.belongsTo % 2 == 0 ? thisElt.eltPos.left > adjacentDir/2 : thisElt.eltPos.left < adjacentDir/2);  
 
 	
-		if (dirSwitch && trigger == false) {								// trigger animations for adjacent container
-			console.log('hello')
+		if (dirSwitch && trigger == false && Object.keys(eltsObject).length > 1) {								// trigger animations for adjacent container
+		//	console.log('hello')
 			trigger = true;
 		 	tempArray = [];
 			
-			for(var i = 0; i < adjacentParentId.length; i++){ 						//Loop the array
-			
-					tempArray.push(Math.abs(adjacentParentId[i].pos.top))
-			
-				
-			}
-			
+			for(var i = 0; i < adjacentParentId.length; i++){ 						//Loop the array			
+					tempArray.push(Math.abs(adjacentParentId[i].pos.top))	
+			}			
 			nToAnimate = tempArray.indexOf(closest(tempArray, thisElt.eltPos.top)); 
-
-			 for(var ind = nToAnimate; ind < adjacentParentId.length; ind++){ 						//Loop the array starting from the first element to be moved down
+			
+			for(var ind = nToAnimate; ind < adjacentParentId.length; ind++){ 						//Loop the array starting from the first element to be moved down
 				var objectNumber = adjacentParentId[ind];
 				
 				if (objectNumber.moved != true) {
 					objectNumber.transition({y: '+=' + elt.completeHeight},200);
 						objectNumber.moved = true;
-						objectNumber.pos.top = objectNumber.pos.top + elt.completeHeight;
-						
-						
-				}
-					
-				
+						objectNumber.pos.top = objectNumber.pos.top + elt.completeHeight;												
+				}			
 			}	 	
 			elt.insertPos = adjacentParentId[nToAnimate].n		
 				
 		};
-		 if (!dirSwitch && trigger == true) {											// go back to original container
+		if (!dirSwitch && trigger == true && Object.keys(eltsObject).length > 1) {											// go back to original container
 			
 			trigger = false;
 			
@@ -141,20 +130,15 @@
 					objectNumber.transition({y: 0},200);
 						objectNumber.moved = false;
 						objectNumber.pos.top = objectNumber.pos.top - elt.completeHeight;
-				}
-					
-				
-			}
-			
-			
-		} 
-		console.log(elt.insertPos)
-				
-		if(!o.isHorizontal && thisElt.eltPos.left != oldPos.left) { 
+				}			
+			}			
+		}  
+		
+		if(!o.isVertical && thisElt.eltPos.left != oldPos.left) { 
 			move = (thisElt.eltPos.left > oldPos.left ? 'forward' : 'backward');		// check whether the move is forward or backward
-			console.log('dsfdsfdfdf')
+		
 		} 
-		else if (o.isHorizontal && thisElt.eltPos.top != oldPos.top) {	
+		else if (o.isVertical && thisElt.eltPos.top != oldPos.top) {	
 			move = (thisElt.eltPos.top > oldPos.top ? "down" : "up");					// check whether the move is down or up													
 		}
 		else {
@@ -166,6 +150,7 @@
 				var eltNext = elts[elt.n + 1];
 				var eltNextBound = eltNext.pos.left + parseInt(eltNext.completeWidth / 2);
 				if(thisElt.eltPos.left + elt.completeWidth > eltNextBound){
+					
 					if (eltNext.hasClass('locked') ){ return; }  						// don't move beyond green colored items for difficulty setting 0
 					elt.insertAfter(eltNext);
 					eltNext.pos.left = elt.pos.left;
@@ -183,8 +168,10 @@
 		else if(move == "backward"){			//  move backward	
 			if(elt.n > 0){
 				var eltPrev = elts[elt.n - 1];
+					
 				var eltPrevBound = eltPrev.pos.left + parseInt(eltPrev.completeWidth / 2);				
-				if(thisElt.eltPos.left < eltPrevBound){								
+				if(thisElt.eltPos.left < eltPrevBound){	
+									
 					if (eltPrev.hasClass('locked') ){ return; } 
 					elt.insertBefore(eltPrev);						
 					elt.pos.left = eltPrev.pos.left;
@@ -201,7 +188,7 @@
 		} 
 		if(move == 'up'){						//  move up
 	
-			  if (trigger) {										// trigger  for animating adjacent container
+			if (trigger) {										// trigger  for animating adjacent container
 				
 				for(var i = 0; i < adjacentParentId.length; i++){ 			//Loop the array
 					var obj = adjacentParentId[i]
@@ -215,8 +202,8 @@
 							obj.pos.top = obj.pos.top + elt.completeHeight;
 							
 						
-						}
-					}
+						};
+					};
 				};
 	
 			}   
@@ -241,10 +228,8 @@
 				}
 			}
 		}
-		else if(move == 'down'){ 				//  move down
-		
-		    if (trigger) {										// trigger  for animating adjacent container
-						
+		else if(move == 'down'){ 				//  move down	
+		    if (trigger) {										// trigger  for animating adjacent container					
 				for(var i = 0; i < adjacentParentId.length; i++){ //Loop the array
 					var obj = adjacentParentId[i]
 					if (ui.position.top  + elt.completeHeight > obj.pos.top + obj.completeHeight/2) {
@@ -262,30 +247,33 @@
 				
 			}   
 			else {
-					if(elt.n < elts.length-1){
-						var eltNext = elts[elt.n + 1];					
-						var eltNextBound = eltNext.pos.top + parseInt(eltNext.completeHeight / 2);
-						if(thisElt.eltPos.top + elt.completeHeight > eltNextBound){
-							if (eltNext.hasClass('locked') ){ return; } 
-							elt.insertAfter(eltNext);
-							eltNext.pos.top = elt.pos.top;
-							elt.pos.top += eltNext.completeHeight;																					
-							elts[elt.n] = eltNext;
-							elts[elt.n + 1] = elt;																
-							elts[elt.n].n = elt.n; 
-							elt.n = elt.n + 1; 
-							eltNext.transition({'top': eltNext.pos.top + 'px', y: '+=' + elt.completeHeight},0, function () {		
-								$(this).transition({y: 0}, 250);						
-							});
-						}
+				if(elt.n < elts.length-1){
+					var eltNext = elts[elt.n + 1];					
+					var eltNextBound = eltNext.pos.top + parseInt(eltNext.completeHeight / 2);
+					if(thisElt.eltPos.top + elt.completeHeight > eltNextBound){
+						if (eltNext.hasClass('locked') ){ return; } 
+						elt.insertAfter(eltNext);
+						eltNext.pos.top = elt.pos.top;
+						elt.pos.top += eltNext.completeHeight;																					
+						elts[elt.n] = eltNext;
+						elts[elt.n + 1] = elt;																
+						elts[elt.n].n = elt.n; 
+						elt.n = elt.n + 1; 
+						eltNext.transition({'top': eltNext.pos.top + 'px', y: '+=' + elt.completeHeight},0, function () {		
+							$(this).transition({y: 0}, 250);						
+						});
 					}
 				}
 			}
+		}
 	};
 	
-	function onStop(evt, ui, elt, div, o)	{									// Stop
+
 	
-		var	outAnim = ( o.isHorizontal && transSupport ? ({x: 0, y: 0, 'box-shadow': 'none'}) : ({ x: 0, y: 0, 'opacity': 1.0, 'z-index':5 }) );
+	function onStop(evt, ui, elt, div, o)	{									// Stop
+		var dropInContainer = (elt.belongsTo % 2 == 0 ? elt.belongsTo +1 : elt.belongsTo - 1); 	//find out if dropped elt belongs to even or odd container and select opposite
+	
+		var	outAnim = ( o.isVertical && transSupport ? ({x: 0, y: 0, 'box-shadow': 'none'}) : ({ x: 0, y: 0, 'opacity': 1.0, 'z-index':5 }) );
 
 		if (o.setChars) {	 setChars(div);	}			// calls the setChars function	
 		
@@ -316,16 +304,20 @@
 			}		
 			else {  													// for difficulty setting 0
 				
-				$(this).transition(outAnim,240, function () {       // auto color lis when difficulty set to 0 - Senner
+				$(this).transition(outAnim,200, function () {       // auto color lis when difficulty set to 0 - Senner
 					if (!!o.autoValidate) {
 						o.autoValidate();	 						// calls the autovalidate function in the plugin calling script
 						
 					}
 					  if (trigger) {
 						
-						elt.hide();
-						adjacentParentId[0].parents(':eq(1)').jumbleScramble('add', elt.text(), elt.insertPos);
-						//$(".jMyPuzzle").eq(1).find('li').eq(elt.insertPos).css('opacity',1)
+						//
+						console.time(name)
+						instanceArr[dropInContainer].addLiElem(elt.text(), elt.insertPos)
+						instanceArr[elt.belongsTo].removeLiElem(elt)
+						console.timeEnd(name)
+				
+						
 						
 						trigger = false;
 						//console.log(adjacentParentId)
@@ -344,66 +336,141 @@
 	};
 	
 	var defaults = {
-		isHorizontal: false,
+		isVertical: false,
 		setChars: false,
 		layoutComplete: function () {}
 	}
 	
 	var eltsObject = {};											// object used for storing and updating data on each ul array with lis
 	
-	function JumbleScramble(element, options, liText, liPosition) {					// Constructor function 
+	var conCount = 0;
+	function JumbleScramble(element, options) {					// Constructor function 
 	
 		this.element = element;
-		this.options = options;
-		if (typeof options === 'string') {
+		this.div = $(this.element);
+		this.ul = $("ul", this.div);
+		this.divId = this.div.attr('id');
+		this.container = conCount;
+		conCount++;
+	
+		/* if (typeof options === 'string') {
 			this.liText = liText;
 			this.liPosition = liPosition;
 			this[options + 'LiElem']()
 		}
-		else {
+		else { */
 			this.options = $.extend( {}, defaults, options) ;
 			this.init();
-		}		
-	};																		
-	
-	
-	JumbleScramble.prototype.addLiElem = function () {								// Add new li to previous collection
-			
-		 	var div = $(this.element), ul = $("ul", div), divId = div.attr('id'), n= Math.min(Math.max(parseInt(this.liPosition), 0), eltsObject[divId].length);
-			o = eltsObject[divId][0].o;		
-	
-			var elt = $("<li class='listItem'>" + this.liText + "</li>");
+		//}		
+	};	
+
+	JumbleScramble.prototype.removeLiElem = function () {							// Remove new li to previous collection
+		
+		
+		var div = this.div; 
+			var ul = this.ul; 
+			var divId = this.divId;
+			var eObjId = eltsObject[divId];	
+			var o = this.options;
+			var elt = arguments[0];
+			var n = elt.index();
+			var thisContainer = this.container;
 			var allBeforeWidth = 0;
 			var allBeforeHeight = 0;
-			var allAfterWidth = 0;
-			var tempArr = []
-			for (var i=0;i<eltsObject[divId].length;i++) { 
-				if (i < n ) {
-					allBeforeWidth += eltsObject[divId][i].completeWidth;
-					allBeforeHeight += eltsObject[divId][i].completeHeight;
+			var tempArr = [];
+			var $thisWidth = (o.isVertical ? 0: elt.outerWidth(true));
+			var $thisHeight = elt.outerHeight(true);
+			var dropInContainer = (elt.belongsTo % 2 == 0 ? elt.belongsTo +1 : elt.belongsTo - 1);
+			
+			console.log(elt.text())
+			for (var i=0;i<eObjId.length;i++) { 
+				if (i <= n ) {
+					allBeforeWidth += eObjId[i].completeWidth;
+					allBeforeHeight += eObjId[i].completeHeight;
+					
 				}	
-				else {	tempArr.push(eltsObject[divId][i])	}
+				else {	
+				tempArr.push(eObjId[i])	
+				
+				}
 			}
 			
-			if (n > 0) {
-				elt.insertAfter( eltsObject[divId][n-1]).css({
-					'left': allBeforeWidth + 'px', 
-					'top' : allBeforeHeight + 'px'
-					});
-			}
-			else {
-				elt.insertBefore( eltsObject[divId][n]).css({
-					'left' : allBeforeWidth + 'px', 
-					'top' : allBeforeHeight + 'px'
-					});
-			}
+			//delete eObjId[n];
+			var eObjIdLenght = eObjId.length -1
+			ul.find('li').eq(n).nextAll().animate({
+				'left': '-=' + $thisWidth + 'px',
+				'top' : '-=' + $thisHeight + 'px',
+				x: '+=' + $thisWidth,
+				y: '+=' + $thisHeight
+				},0).promise().done(function () {
+					console.log('animating')
+					$(this).animate({x: 0, y: 0}, 200)
+					
+				});
+			ul.css({
+				'width' : parseInt( ul.css('width')) - parseInt($thisWidth) + 'px',
+				'height' : parseInt( ul.css('height')) - parseInt($thisHeight) + 'px'
 		
-			var $thisWidth = (o.isHorizontal ? 0: elt.outerWidth(true));
+			})
+			i = 0;	
+			 for (var i=0;i<tempArr.length;i++) { 
+				tempArr[i].pos.left = tempArr[i][0].offsetLeft;
+				tempArr[i].pos.top =  tempArr[i][0].offsetTop;							
+				tempArr[i].n = tempArr[i].n -1;
+				eObjId[n + i] = tempArr[i];
+				
+				//				
+				
+			}
+			delete eObjId[eObjId.length -1]
+			eObjId.length = eObjIdLenght
+			elt.remove()
+			console.log(eltsObject)
+			
+			
+			
+			
+			
+			
+			
+	};	
+	
+	
+	JumbleScramble.prototype.addLiElem = function (liText, liPosition) {								// Add new li to previous collection
+			
+		 	var div = this.div; 
+			var ul = this.ul; 
+			var divId = this.divId;
+			var eObjId = eltsObject[divId];
+			var n = Math.min(Math.max(parseInt(liPosition), 0), eObjId.length);
+			var o = this.options;
+			var elt = $("<li class='listItem'>" + liText + "</li>");
+			var thisContainer = this.container;
+			var allBeforeWidth = 0;
+			var allBeforeHeight = 0;
+			var tempArr = [];
+			
+			for (var i=0;i<eObjId.length;i++) { 
+				eObjId[i].moved = false;
+				if (i < n ) {
+					allBeforeWidth += eObjId[i].completeWidth;
+					allBeforeHeight += eObjId[i].completeHeight;
+				}	
+				else {	tempArr.push(eObjId[i])	}
+			}
+			
+			var eltObj = {
+				'left': allBeforeWidth + 'px', 
+				'top' : allBeforeHeight + 'px'
+			}
+			if (n > 0 ? elt.insertAfter( eObjId[n-1]).css(eltObj) : elt.insertBefore( eObjId[n]).css(eltObj) );
+			
+			var $thisWidth = (o.isVertical ? 0: elt.outerWidth(true));
 			var $thisHeight = elt.outerHeight(true);
 			
 			if (trigger) {
 				for(var i = 0; i < adjacentParentId.length; i++){
-					var style = window.getComputedStyle(adjacentParentId[i].get(0));  // Need the DOM object
+					var style = window.getComputedStyle(adjacentParentId[i].get(0));  
 					var matrix = new WebKitCSSMatrix(style.webkitTransform);
 
 					if (matrix.m42 != 0) {adjacentParentId[i].transition({y:0},0)} 
@@ -420,7 +487,7 @@
 				
 			})
 					
-			addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o)
+			addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o, thisContainer)
 			addHandlers(eltsObject, divId, elt, o, div);
 			
 			
@@ -428,28 +495,26 @@
 				tempArr[i].pos.left = tempArr[i][0].offsetLeft;
 				tempArr[i].pos.top =  tempArr[i][0].offsetTop;							
 				tempArr[i].n = n + i+1;
-				eltsObject[divId][n + 1 + i] = tempArr[i];			
+				eObjId[n + 1 + i] = tempArr[i];			
 			}
 				
 	}
 
-	JumbleScramble.prototype.removeLiElem = function () {							// Remove new li to previous collection
-		
-			
-	};
+	
 		
 	
 	JumbleScramble.prototype.init = function () {
 		
 		var o = this.options; 
-		var div = $(this.element), ul = $("ul", div), li = $("li", ul), divId = div.attr('id');						// Variables declaration
-		var left=0, top = 0, n = 0, ulSize = 0;;	
+		var div = this.div, ul = this.ul, li = $("li", ul), divId = this.divId;						// Variables declaration
+		var left=0, top = 0, n = 0, ulSize = 0;	
+		var thisContainer = this.container;	
 		eltsObject[(divId.toString())] = new Array(li.size());
 	
 		li.each(function(liInd, liElem){ 											// Loop over each li, position, store object data and bind draggable handlers. 
 			var elt = $(this);	
 			
-			if (o.isHorizontal) {
+			if (o.isVertical) {
 				elt.css('top', top + 'px');											// get each li height in case of individual heights.
 				var $thisHeight = elt.outerHeight(true);
 				top += $thisHeight;	
@@ -464,12 +529,12 @@
 				ulSize += $thisWidth; 												// calculate the size of the ul element				
 			}	
 			
-			addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o);		
+			addToObject(eltsObject, divId, elt, n, $thisHeight, $thisWidth, o, thisContainer);		
 			addHandlers(eltsObject, divId, elt, o, div);			
 			n = n+1;		
 		});
 			
-		if (!o.isHorizontal) {	
+		if (!o.isVertical) {	
 			ul.css({width:ulSize, height: li.outerHeight(true) + 'px' });	 		// Update the ul size	
 		}
 		else {
@@ -477,15 +542,33 @@
 		}
 		//console.log(eltsObject)	
 	};
-			
+	var instanceArr = [];	
+	
 	$.fn.jumbleScramble = function(options, arg1, arg2) { 							// jumbleScramble fn
 		
-		return this.each(function () {
-			new JumbleScramble(this, options, arg1, arg2) 	
-		}).promise().done(function (){
-			if (!!options.layoutComplete )options.layoutComplete();
+			
+		if (typeof options === 'string') {	
+			var self = this;
+			var thisId  = ( options == 'remove' ? this.parent().parent().attr('id') : this.attr('id') );
+			
+			$.each(instanceArr, function (i,e){
+				if (thisId == this.element.attributes.id.nodeValue) {
+				instanceArr[i][options + 'LiElem'](arg1 || self, arg2)  // self is for the remove method, arg2 is then undefined.
+				}
+			});	
+			
+		}
+		else {
+			return this.each(function (i,e) {
 				
-		});
-	};																			
+				instanceArr.push(new JumbleScramble(this, options, arg1, arg2)) 	
+			}).promise().done(function (){
+				if (!!options.layoutComplete )options.layoutComplete();
+					//console.log(instanceArr)
+			});			
+		}
+	
+	};	
+
 
 })(jQuery);
--- jsQuiz/sortable.js
+++ jsQuiz/sortable.js
@@ -34,9 +34,9 @@ var $h1 = $('h1');
     };
 })(jQuery);
 			
-			var isHorizotal = true;
+			var isVertical = true;
 			
-			if (isHorizotal == false) {
+			if (isVertical == false) {
 				$('div .jMyPuzzle').removeAttr('style').css('height', '50px')
 				$('.listItem').css({
 					width: 'auto',
@@ -57,11 +57,12 @@ var $h1 = $('h1');
 			
 			
 			$(".jMyPuzzle").jumbleScramble({
-				isHorizontal: isHorizotal,
+				isVertical: isVertical,
 				layoutComplete: function () {	
 				
-						 $(".jMyPuzzle").eq(0).jumbleScramble('add', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et mi sapien.', 3) 
-						//$(".jMyPuzzle").eq(1).jumbleScramble('add', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et mi sapien.', 3)  
+					//	 $(".jMyPuzzle").eq(0).jumbleScramble('add', 'Lorem ipsum dolor.', 6) 
+						//$(".jMyPuzzle").eq(1).jumbleScramble('add', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et mi sapien.', 3) 
+					//	$(".jMyPuzzle").eq(0).find('li').first().jumbleScramble('remove') 
 				}						
 			});	
 
@@ -74,131 +75,5 @@ var $h1 = $('h1');
 
 		var score = 0;
 		
-		$('#check').click(function (event) {
-				$(this).css('text-decoration','line-through').attr('disabled', 'disabled');
-				event.preventDefault();
-				var staticLi = $('#static').find('li');
-				$('.imgPos').css({opacity:0});
-				$splitList.find('li').each(function () {
-					var $this = $(this);
-						var index = $this.index();
-			
-						if ( completeString.indexOf(staticLi.eq(index).text() + $this.text()) >= 0  ) {
-							
-							$this.addClass('ui-state-disabled').find('.imgPos').attr('src','css/cssImg/checkButtonBlack.png');
-							//$this.add(staticLi.eq(index)).css({color:'green'});
-							score = score + 10;
-							
-						}
-						else {
-							/* console.log('hello') */
-							//$this.find('.imgPos').attr('src','css/cssImg/cancelButtonSmall.png');
-						}
-						if ($this.hasClass('ui-state-disabled') == true) {
-						//	window.parent.globalScoreVariable = window.parent.globalScoreVariable + 10;
-						}
-									
-					}) 	
-				$splitList.find('li').each(function (index2) {
-				var $this = $(this);
-					var index = $this.index();
 		
-						if ( completeString.indexOf(staticLi.eq(index).text() + $this.text()) >= 0  ) 
-						{
-							 $this
-							.find('.imgPos').delay(index2 * 90).transition({opacity:1}).end() 
-							.add(staticLi.eq(index)).delay(index2 * 100)
-							.transition({
-										
-							},  80, function () {
-							$this.add(staticLi.eq(index)).css({
-							//color:'#006400',
-							//backgroundColor: 'rgb(187, 187, 187)'
-							})
-							});
-						//	$this.add(staticLi.eq(index)).css({color:'green'}); 
-							
-							
-						}
-						else {
-						
-						
-						/* $this
-						.find('.imgPos').delay(index2  * 90).transition({opacity:1}).end()
-						.add(staticLi.eq(index)).delay(index2 * 120).transition({ */
-						
-					
-						$this.add(staticLi.eq(index)).delay(index2 * 100).transition({ 
-						
-						
-							},  0, function () {
-								$(this).find('.squiggle').squiggle({
-									intensity:70,
-									thickness:3,
-									color: 'rgb(34, 34, 34)'										
-								});
-								
-								});
-						}
-						
-				}).promise().done(function () {
-					$feedText.transition({y: '-10px', opacity: 0}, function () {
-							$(this).css({y: '10px'});
-							if (score == 100) { $(this).html('Well done! Your score is ' + score + ' out of 100'); }
-							if (score >= 70 && score < 100) { $(this).html('Almost. Your score is ' + score + ' out of 100'); }
-							if (score < 70 ) { $(this).html('Try again. Your score is ' + score + ' out of 100'); } 
-						$(this).transition({y: '0px', opacity: 1});
-					});
-					
-				})
-			
-				//$scoreText.text('Score: ' + window.parent.globalScoreVariable + ' Out of 100').hide().fadeIn();
-		
-				
-				$splitList.find('li').draggable('disable');
-				
-			});
-
-		$('#restart').click(function(event) {
-			
-			$('#check').css('text-decoration','none');
-			event.preventDefault();
-			score = 0;
-				
-			var $splitListLi =  $splitList.find('li');		
-			$('#allLists').transition({opacity: 0}, 400, function() {
-				$('li').css({
-			//	color:'rgb(34, 34, 34)'
-				});
-				randomNumbers = shuffle(answers);
-				$splitListLi.each(function(i){
-						
-						$(this).html("<span class='squiggle'>" + randomNumbers[i] + '</span>')
-						.append("<div class='imgClass'><img class='imgPos' src='css/cssImg/handleSmall.png'></div>");
-						
-				});
-				randomNumbers = shuffle(questions);				
-				$static.find('li').each(function(i){
-					$(this).html("<span class='squiggle'>" + randomNumbers[i] + '</span>')
-					});
-			}).transition({opacity: 1}, 700, function () {
-				
-				$feedText.transition({y: '-10px', opacity: 0}, function () {
-					$(this).css({y: '10px'});
-					if (alignCenter == 'true') {
-						$(this).html('Sort the words or sentences to match those on the left side.').transition({y: '0px', opacity: 1});
-					}
-					else {
-						$(this).html('Sort the endings to create correct and meaningful sentences.').transition({y: '0px', opacity: 1});
-					}
-				});
-			});
-		
-		
-			$splitList.find('li').draggable('enable');
-
-			$check.removeAttr('disabled'); 
-	
-		});
-
  });
